9dacca7286bc07d6245308d35e8c8403860a38c1
程瑜 <2479676487@qq.com>
2023-06-12 15:17:27 +0800

    test push feat

--- src/main/java/com/codeep/dbproject/DbProjectApplication.java
+++ src/main/java/com/codeep/dbproject/DbProjectApplication.java
@@ -1,8 +1,10 @@
 package com.codeep.dbproject;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.codeep.")
 @SpringBootApplication
 public class DbProjectApplication {
 
